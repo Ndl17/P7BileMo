9efdea2ee16de6dfdd453bfdceaa464ba951eb43
Ndl17 <juliengautiernadal@hotmail.com>
2023-07-13 16:31:21 +0200

    phone controller + first route
    2 route get allphone et detailed
    (non sécu pour l'instant)

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -2,18 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Phone;
+use App\Repository\PhoneRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class PhoneController extends AbstractController
 {
     #[Route('/api/phones', name: 'phones', methods: ['GET'])]
-    public function getAllPhones(): JsonResponse
+public function getAllPhones(PhoneRepository $phoneRepository, SerializerInterface $serializer): JsonResponse
     {
-        return new JsonResponse([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PhoneController.php',
-        ]);
-    }
+    $phones = $phoneRepository->findAll();
+    $jsonPhonesList = $serializer->serialize($phones, 'json');
+    return new JsonResponse($jsonPhonesList, Response::HTTP_OK, [], true);
+
+}
+
+
+#[Route('/api/phones/{id}', name: 'detailPhone', methods: ['GET'])]
+function getDetailPhone(Phone $phone, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonPhonesList = $serializer->serialize($phone, 'json');
+        return new JsonResponse($jsonPhonesList, Response::HTTP_OK, [], true);
+    
+}
 }
