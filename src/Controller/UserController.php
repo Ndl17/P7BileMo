ec04274f8474e8da57fef2790e2f78176c47b8c8
Ndl17 <juliengautiernadal@hotmail.com>
2023-07-20 12:18:38 +0200

    route user creation (liste & detail)

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): JsonResponse
+    #[Route('/api/users', name: 'users', methods: ['GET'])]
+/**
+ * Route pour récupérer tous les utilisateurs
+ * @param \App\Repository\UserRepository $userRepository
+ * @param \Symfony\Component\Serializer\SerializerInterface $serializer
+ * @return \Symfony\Component\HttpFoundation\JsonResponse
+ */
+public function getAllUsers(UserRepository $userRepository, SerializerInterface $serializer): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
-    }
+    $users = $userRepository->findAll();
+    $jsonUserList = $serializer->serialize($users, 'json', ['groups' => 'getUsers']);
+    return new JsonResponse($jsonUserList, Response::HTTP_OK, [], true);
+}
+
+
+#[Route('/api/users/{id}', name: 'detailUser', methods: ['GET'])]
+
+/**
+ * Route pour récupérer un utilisateur par son id
+ * @param \App\Entity\User $user
+ * @param \Symfony\Component\Serializer\SerializerInterface $serializer
+ * @return \Symfony\Component\HttpFoundation\JsonResponse
+ */
+function getDetailUser(User $user, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonUserList = $serializer->serialize($user, 'json', ['groups' => 'getUsers']);
+        return new JsonResponse($jsonUserList, Response::HTTP_OK, [], true);
+    
+}
+
 }
